a169db5586cfa45ca71a42eea8feda341d035d44
boram <boram@innerviewit.com>
2023-12-11 13:33:47 +0900

    과제 수정

--- banner/main.js
+++ banner/main.js
@@ -12,61 +12,55 @@ $(document).ready(function(){
                 disableOnInteraction: false,
             },
             navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+                nextEl: '.monze_guide_section .swiper-button-next',
+                prevEl: '.monze_guide_section .swiper-button-prev',
             },
             pagination: {
-                el: '.swiper-pagination',
+                el: '.monze_guide_section .swiper-pagination',
                 type: 'fraction',
             },
         });
-        guideSwiper.on('reachEnd', function(){
-            setTimeout(function(){
-            	guideSwiper.slideToLoop(0, 1000, false);
-            }, 4000);
-            
-        });
+       
         
-        if ( !!$('.tabcont .guide_slide:first-child').attr('data-title') ) {
-            
-        	var tabTitles = $('.tabcont .guide_slide:first-child').attr('data-title').split(',');
-            $('.tabcont .guide_slide:first-child').remove();
-            $('.monze_guide_section').css('opacity', 1);
-            
-            tabTitles.forEach(function(title, index){
-                var tabItemCopy = $('.monze_guide_section .tab_item:first-child').clone(false);
-                tabItemCopy.text(title).appendTo('.monze_guide_section .tab_list');
-                tabCont['#'+ (index + 1)] = [];
-            });
-            $('.monze_guide_section .tab_item:first-child').addClass('on');
-        }
-        
-        
+        $('.monze_guide_section').css('opacity', 1);
     	$('.tabcont .swiper-slide').each(function(){
             var $this = $(this);
         	var slideDataTitle = $(this).attr('data-title');
             var slideTitle;
             var slideText = $(this).find('.guide_text');
             
-            var hashReg = /(#[0-9])/g;
+            var hashReg = /(#[^#$]+)/g;
             slideTitle = slideDataTitle.replaceAll(hashReg,'');
             slideText.html(slideTitle);
             
             var slideHash = slideDataTitle.match(hashReg);
+            
             if(!!slideHash) {
-            	slideHash.forEach(function(hashNum, index){
-                	tabCont[hashNum].push($this.index());
+            	slideHash.forEach(function(hashTitle, index){
+                	if(!tabCont[hashTitle]) {
+                    	tabCont[hashTitle] = [];
+                        tabCont[hashTitle].push($this.index());
+                    } else {
+                        tabCont[hashTitle].push($this.index());
+                    }
                 });
             }
         });
+        if(Object.keys(tabCont).length >= 1) {
+        	Object.keys(tabCont).forEach(function(title, index){
+            	var titleSpan = $('<span>' + title +'</span>');
+                var tabItemCopy = $('.monze_guide_section .tab_item:first-child').clone(false);
+                tabItemCopy.html(titleSpan).appendTo('.monze_guide_section .tab_list');
+            });
+        }
+        $('.monze_guide_section .tab_item:first-child').addClass('on');
         
         
         $('.tab_item').click(function(){
-        	var tabIndex = '#' + $(this).index();
-            console.log('click',tabIndex);
+        	var tabIndex = $(this).text();
             $(this).addClass('on').siblings().removeClass('on');
             var guideSlide = $('.guide_slide');
-			if(tabIndex == '#0') {
+			if(tabIndex == '#전체') {
             	guideSlide.removeClass('displaynone');
             } else {
             	var slideIndex = tabCont[tabIndex];
